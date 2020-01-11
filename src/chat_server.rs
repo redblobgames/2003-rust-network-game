d1c0a710a8a0f61b7518efcbc20b2ce93d3c8600
Amit Patel <redblobgames@gmail.com>
2020-01-11 13:22:06 -0800

    Send messages from the simulation thread to the network thread
    * Make the simulation thread track the network threads by ID.
    * Make the simulation thread send replies to the network threads.
    * Make the network thread read from the message queue, and forward
      that to the client.

--- src/chat_server.rs
+++ src/chat_server.rs
@@ -2,6 +2,7 @@ mod common;
     
 #[cfg(target_arch = "x86_64")]
 mod server {
+    use std::collections::HashMap;
     use std::{thread, time};
     use std::net::TcpListener;
     use std::sync::mpsc;
@@ -15,6 +16,7 @@ mod server {
     }
 
     enum Reply {
+        ChatText(String),
     }
     
     pub fn run() {
@@ -23,12 +25,24 @@ mod server {
         let (sim_tx, sim_rx) = mpsc::channel::<Request>();
 
         thread::spawn(move || {
+            let mut channels = HashMap::new();
             println!("SIM: begin");
             for request in sim_rx {
                 match request {
-                    Request::Connect(addr, _net_tx) => println!("SIM: connect from {}", addr), /* TODO: save the net_tx in a map */
-                    Request::Player(addr, Message{text}) => println!("SIM: message from {}: {}", addr, text),
-                    Request::Disconnect(addr) => println!("SIM: disconnect from {}", addr), /* TODO: remove corresponding net_tx */
+                    Request::Connect(addr, net_tx) => {
+                        println!("SIM: connect from {}", addr);
+                        channels.insert(addr, net_tx);
+                    },
+                    Request::Player(addr, Message{text}) => {
+                        println!("SIM: message from {}: {}", addr, text);
+                        for (_addr, net_tx) in &channels {
+                            net_tx.send(Reply::ChatText(format!("[{}] {}", addr, text))).unwrap();
+                        }
+                    },
+                    Request::Disconnect(addr) => {
+                        println!("SIM: disconnect from {}", addr);
+                        channels.remove(&addr);
+                    },
                 };
             }
             println!("SIM: end");
@@ -40,16 +54,16 @@ mod server {
             stream.set_read_timeout(Some(socket_timeout)).unwrap();
             let sim_tx = sim_tx.clone();
             let addr = format!("{:?}", stream.peer_addr().unwrap());
-            let (net_tx, _net_rx) = mpsc::channel::<Reply>();
+            let (net_tx, net_rx) = mpsc::channel::<Reply>();
             
             thread::spawn(move || {
                 let mut websocket = tungstenite::accept(stream).unwrap();
                 sim_tx.send(Request::Connect(addr.clone(), net_tx)).unwrap();
 
-                let reply = Message { text: String::from("hello from server") };
-                let encoded = bincode::serialize(&reply).unwrap();
-                websocket.write_message(tungstenite::Message::Binary(encoded)).unwrap();
-                
+                // This loop handles both player events (from the
+                // network, over websocket) and simulation events
+                // (from the simulation thread, over the message
+                // passing queue)
                 loop {
                     match websocket.read_message() {
                         Ok(msg) if msg.is_binary() => {
@@ -64,13 +78,22 @@ mod server {
                             println!("received unexpected msg {:?}", msg);
                         },
                         Err(tungstenite::Error::Io(ref e)) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                            // TODO: This gives me a chance to check the message queue!!
-                            println!("{:?} no bytes to read", time::SystemTime::now());
+                            // The socket read timeout gives me a chance to check the message queue
                         },
                         Err(err) => {
                             println!("{:?} ERRRRRRRR", err);
                             break;
                         },
+                    }
+
+                    for message in net_rx.try_iter() {
+                        match message {
+                            Reply::ChatText(text) => {
+                                let reply = Message{text};
+                                let encoded = bincode::serialize(&reply).unwrap();
+                                websocket.write_message(tungstenite::Message::Binary(encoded)).unwrap();
+                            },
+                        };
                     }
                 }
                 sim_tx.send(Request::Disconnect(addr)).unwrap();
